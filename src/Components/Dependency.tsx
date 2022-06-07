e4bbc3a4afd5507483866642e5cf89bb442a4d34
Zytha <yangyong080228@gmail.com>
2022-06-07 21:18:31 +0800

    Added: Dependecies List

---
+++ src/Components/Dependency.tsx
@@ -0,0 +1,21 @@
+function Dependency(props) {
+    return (
+        <>
+            <a
+                href={props.href}
+                target="_blank"
+                class="flex flex-col justify-center items-center p-8 gap-4 bg-[#242424] rounded-xl drop-shadow-xl"
+            >
+                <img
+                    src={props.image}
+                    alt={props.alt}
+                    height={125}
+                    width={125}
+                />
+                <h1 class="text-4xl font-bold text-[#aeaeae]">{props.text}</h1>
+            </a>
+        </>
+    );
+}
+
+export default Dependency;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,53 @@
 import Navbar from "../Components/Navbar";
+import Dependency from "../Components/Dependency";
+
+// Images
+import SolidJS from "../Images/solidjs.svg";
+import ViteJS from "../Images/vitejs.svg";
+import TailwindCSS from "../Images/tailwindcss.svg";
+import TypeScript from "../Images/typescript.svg";
+import FontAwesome from "../Images/fontawesome.svg";
 
 function About() {
     return (
         <>
             <Navbar />
-            <div class="flex justify-center items-center h-[85vh]">
-                <h1 class="text-6xl font-bold text-[#aeaeae]">About</h1>
+            <div class="flex flex-col justify-center items-center h-[85vh]">
+                <h1 class="text-6xl font-bold text-[#aeaeae] py-8">
+                    Dependencies Used
+                </h1>
+                <div class="flex flex-wrap justify-evenly items-center gap-4">
+                    <Dependency
+                        text="SolidJS"
+                        image={SolidJS}
+                        alt="SolidJS"
+                        href="https://solidjs.com"
+                    />
+                    <Dependency
+                        text="ViteJS"
+                        image={ViteJS}
+                        alt="ViteJS"
+                        href="https://vitejs.dev"
+                    />
+                    <Dependency
+                        text="Tailwind"
+                        image={TailwindCSS}
+                        alt="TailwindCSS"
+                        href="https://tailwindcss.com"
+                    />
+                    <Dependency
+                        text="TypeScript"
+                        image={TypeScript}
+                        alt="TypeScript"
+                        href="https://www.typescriptlang.org"
+                    />
+                    <Dependency
+                        text="FontAwesome"
+                        image={FontAwesome}
+                        alt="FontAwesome"
+                        href="https://fontawesome.com"
+                    />
+                </div>
             </div>
         </>
     );
